9ee0ac9b71355dc54afac499ee1d0d0636341326
ruafem4 <ZXcvbn5890>
2018-09-24 19:19:28 +0300

    Test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 
 public class Hello {
     public static void main(String []arg) {
-        System.out.println("Hello world");
+        System.out.println("Калькулятор сложения");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число:");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число:");
+        int b = scanner.nextInt();
+        System.out.println("Результат сложения двух чисел "+ a +" и "+ b +" = "+(a+b));
+        scanner.close();
     }
 }
