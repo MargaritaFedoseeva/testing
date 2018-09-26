481dc720762595195d287486e12abfbddd2fd37c
ruafem4 <ZXcvbn5890>
2018-09-26 20:43:51 +0300

    Test

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] arg) {
+        Operacionist op = new Operacionist(3,4);
+        System.out.println(op.getSum());
+        System.out.println(op.getSum().getOpType());
+        System.out.println(op.getSum().getC());
+        System.out.println(op.getSum().isFlag());
+    }
+}
