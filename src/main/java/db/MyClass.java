8e9c0df48f643461d5e61d9cb080fa6de26f7ca4
ruafem4 <ZXcvbn5890>
2018-10-24 19:38:18 +0300

    Test

--- src/main/java/db/MyClass.java
+++ src/main/java/db/MyClass.java
@@ -1,26 +1,69 @@
 package db;
 
+import javafx.scene.chart.PieChart;
+
+import java.security.SecureRandom;
 import java.sql.*;
-import java.util.List;
-
-
 
 
 public class MyClass {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Class.forName("org.postgresql.Driver");
 
-        String server="s-msk-t-ver-db1";
-        String port="5432";
-        String database="testdb";
+        String server = "s-msk-t-ver-db1";
+        String port = "5432";
+        String database = "testdb";
 
-        Connection connection;
+        Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://"+server+":"+port+"/"+database,
-                    "trainee","123456");
+            connection = DriverManager.getConnection("jdbc:postgresql://" + server + ":" + port + "/" + database,
+                    "trainee", "123456");
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+        System.out.println("Таблица person");
+        Statement stmt = connection.createStatement();
+
+        String strSelect = "select person_code,first_name,last_name,hiredate from person";
+        ResultSet rset = stmt.executeQuery(strSelect);
+
+        int rowCount = 0;
+        while (rset.next()) { // движение курсора
+            String person_code = rset.getString("person_code");
+            String first_name = rset.getString("first_name");
+            String last_name = rset.getString("last_name");
+            String hiredate = rset.getString("hiredate");
+            System.out.println(person_code + ":  " + first_name + " " + last_name + " /" + hiredate);
+            ++rowCount;
+        }
+        System.out.println("Таблица purchase");
+        String strSelect_2 = "select product_name,salesperson,purchase_date,quantity from purchase";
+        ResultSet rset_2 = stmt.executeQuery(strSelect_2);
+
+        Select(rset_2);
+
+
+        String strInsert = "INSERT INTO purchase (product_name,salesperson,purchase_date,quantity) VALUES ('Small Widget', 'BB', '2018-10-22', 5.00)";
+        System.out.println("The SQL query is: " + strInsert); // Echo for debugging
+        int countInsert= stmt.executeUpdate(strInsert);
+
+        connection.close();
+
+
+     //   Select(rset_2);
+
+    }
+
+    private static void Select(ResultSet rset_2) throws SQLException {
+        int rowCount_2 = 0;
+        while (rset_2.next()) { // движение курсора
+            String product_name = rset_2.getString("product_name");
+            String salesperson = rset_2.getString("salesperson");
+            String purchase_date = rset_2.getString("purchase_date");
+            String quantity = rset_2.getString("quantity");
+            System.out.println(salesperson + ":  " + product_name + " /" + purchase_date + "/  " + quantity);
+            ++rowCount_2;
         }
     }
 }
--- src/main/java/web_server/Class.java
+++ src/main/java/web_server/Class.java
@@ -1,9 +1,11 @@
 package web_server;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
 import javax.xml.ws.Response;
 
 public class Class {
     public static void main(String[] args){
-        Response rs=get("http://www.cbr.ru/scripts/XML_daily.asp");
+       Response rs= (Response) get("http://www.cbr.ru/scripts/XML_daily.asp");
     }
 }
